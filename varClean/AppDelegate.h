0bd9b0f1982914b21aabc8b4aa81d9f29135320e
m1337v <xxmichaelsxx@googlemail.com>
2024-03-01 12:27:24 +0100

    merge testing

---
+++ varClean/AppDelegate.h
@@ -0,0 +1,14 @@
+#import <UIKit/UIKit.h>
+#include "varClean.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
++ (id)getDefaultsForKey:(NSString*)value;
++ (void)setDefaults:(NSObject*)value forKey:(NSString*)key;
+
+@end
+
+#define Localized(x) NSLocalizedString(x,nil)
+
